3070cf3f8c7da9d362f2af211c89c7ed13533620
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2023-04-23 14:44:46 +0300

    equals & hashcode

--- src/main/java/com/orcrist/account/model/Customer.kt
+++ src/main/java/com/orcrist/account/model/Customer.kt
@@ -10,8 +10,29 @@ data class Customer(
         @GeneratedValue(generator = "UUID")
         @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
         val id: String?,
+        val name: String?,
+        val surname : String?,
 
         @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
         val transaction: Set<Account>?
 
-)
+){
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as Customer
+
+                if (id != other.id) return false
+                if (name != other.name) return false
+                if (surname != other.surname) return false
+                return transaction == other.transaction
+        }
+
+        override fun hashCode(): Int {
+                var result = id?.hashCode() ?: 0
+                result = 31 * result + (name?.hashCode() ?: 0)
+                result = 31 * result + (surname?.hashCode() ?: 0)
+                return result
+        }
+}
